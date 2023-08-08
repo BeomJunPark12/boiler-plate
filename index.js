98b5b9ee05b427850f02c362f588bae349e52191
beomjunpark <pbeomjun12@gmail.com>
2023-08-08 19:23:27 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./confilg/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://pbeomjun12:9Nx0MgvgQjIf5eHH@cluster0.wbimmfg.mongodb.net/"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("안녕하세요!");
+});
+
+app.post("/register", async (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user
+    .save()
+    .then((userInfo) => res.status(200).json({ success: true }))
+    .catch((err) => res.json({ success: false, err }));
 });
 
 app.listen(port, () => {
